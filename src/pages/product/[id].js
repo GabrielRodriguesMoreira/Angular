4af824cd0f4eb187c1f32149e357925a1facb26a
GabrielRodriguesMoreira <80790674+GabrielRodriguesMoreira@users.noreply.github.com>
2023-05-10 19:42:35 -0300

    init

--- src/pages/product/[id].js
+++ src/pages/product/[id].js
@@ -1,10 +1,8 @@
 const Product = () => {
-  const router = useRouter();
-  const { product } = router.query;
 
   return (
-    <h1>id: {product}</h1>
-  );
+    <h1>id:</h1>
+  )
 };
 
 export default Product;
